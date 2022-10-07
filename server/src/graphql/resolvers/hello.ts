5a9c3a909423e92fa7fe864eb81d76dc431fa154
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-10-07 18:16:35 -0500

    database

--- server/src/graphql/resolvers/hello.ts
+++ server/src/graphql/resolvers/hello.ts
@@ -1,7 +1,20 @@
+import { prisma } from '../../index';
+
 export default {
   Query: {
     hello: async (_: any) => {
       return 'hello from graphql';
+    }
+  },
+  Mutation: {
+    addHello: async (_: any, { content }: { content: string }) => {
+      await prisma.hello.create({
+        data: {
+          content: `hello ${content}`
+        }
+      });
+
+      return 'success';
     }
   }
 };
