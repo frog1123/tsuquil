a1b4cef1570c38b265a07442632d52a14850ea40
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-10-06 19:53:02 -0500

    +

---
+++ client/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  prefix: 'tw-',
+  content: ['./pages/**/*.tsx', './components/**/*.tsx'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
