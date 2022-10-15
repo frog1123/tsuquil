3d1fbd4232138cca39f59503eb9bcd32168db36a
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-10-15 17:15:35 -0500

    +

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -4,12 +4,25 @@ import { ApolloProvider } from '@apollo/client';
 import client from '../apollo-client';
 import UserContext from '../UserContext';
 import { useState } from 'react';
+import ProgressBar from '@badrap/bar-of-progress';
+import Router from 'next/router';
 
 const App = ({ Component, pageProps }: AppProps) => {
   const [value, setValue] = useState({
     reloadPostsList: false,
     setPermPosts: false
   });
+
+  const { start, finish } = new ProgressBar({
+    size: '2px',
+    color: '#5ee6ae',
+    className: 'progress',
+    delay: 100
+  });
+
+  Router.events.on('routeChangeStart', start);
+  Router.events.on('routeChangeComplete', finish);
+  Router.events.on('routeChangeError', finish);
 
   return (
     <ApolloProvider client={client}>
